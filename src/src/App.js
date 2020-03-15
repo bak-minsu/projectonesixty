a6132df9476b5d94810aa6359200a34b3e900ac1
Minsu Park <minsupark@live.com>
2020-03-15 00:12:47 -0400

    Added an about page

--- src/src/App.js
+++ src/src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 import camera from './img/camera.png';
 import flower from './img/flower.png';
+import duck from './img/duck.png';
 import { FaHome, FaHotel, FaArrowUp } from 'react-icons/fa';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <MainContent/>
-      <GoTopButton/>
+      <Router>
+        <NavBar/>
+        <Switch>
+          <Route exact path="/" component={MainContent}/>
+          <Route exact path="/about" component={About}>
+          </Route>
+        </Switch>
+        <GoTopButton/>
+        <Footer/>
+      </Router>
     </div>
   );
 }
@@ -17,18 +31,22 @@ function App() {
 function NavBar() {
   return (
     <nav id="nav" className="border">
+      <h1>HST 160 What-If Project</h1>
+      <p>By Naomi Henderson, Timothy Luft, Ryan McCauley, Min Park, Rachel Yao</p>
       <NavButton icon={<FaHome/>} text={"Home"} route={"/"}/>
-      <NavButton icon={<FaHotel/>} text={"About"} route={"/"}/>
+      <NavButton icon={<FaHotel/>} text={"About"} route={"/about"}/>
     </nav>
   )
 }
 
 function NavButton({text, route, icon}) {
   return (
-    <a className="nav-button" href={route}>
-      {icon}
-      <p>{text}</p>
-    </a>
+    <Link to={route}>
+      <div className="nav-button">
+        {icon}
+        <p>{text}</p>
+      </div>
+    </Link>
   )
 }
 
@@ -39,19 +57,6 @@ function ContentDiv({children, otherClasses}) {
       {children}
     </div>
   )
-}
-
-function MainContent() {
- return (
-  <ContentDiv otherClasses={"main-content border"}>
-    <Title>What If?</Title>
-    <TextContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in malesuada libero, eget laoreet lectus. Aliquam finibus dapibus ligula, sit amet tincidunt dui posuere non. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer porttitor tempus tortor ut dictum. Maecenas placerat orci sit amet nisi luctus, porta euismod ipsum sagittis. Nullam nec malesuada diam. Suspendisse eget ante urna.</TextContent>
-    <Image image={camera} alt={"camera"}/>
-    <TextContent>Ut aliquet felis feugiat lorem posuere, id vulputate leo fermentum. Suspendisse vestibulum dolor odio, vitae sollicitudin quam mattis id. Etiam eget eleifend est. Aliquam at iaculis justo. Aenean pulvinar molestie eleifend. Aliquam egestas dignissim mollis. Suspendisse semper risus quis neque ultrices ultricies.</TextContent>
-    <Image image={flower} alt={"flower"}/>
-    <TextContent>Nunc at magna eleifend, luctus lorem vel, bibendum odio. Quisque egestas quam faucibus libero cursus rutrum. Sed a metus placerat, pulvinar nisi at, finibus urna. Duis malesuada quis felis vel tempus. Morbi massa ex, aliquet eget erat eu, tincidunt pulvinar nunc. Quisque lorem est, sollicitudin eget sagittis vitae, faucibus aliquam sapien. Pellentesque vulputate neque diam, ac cursus lorem commodo nec. Curabitur et mauris vitae felis fermentum iaculis. Sed eu urna at nibh convallis sagittis ut eget neque. Vivamus fringilla mauris blandit felis maximus, vel efficitur nisi condimentum. Aliquam vel purus augue. Fusce nulla quam, tempor ac sagittis vel, luctus at quam. Mauris sagittis ac sem vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam egestas urna risus, sit amet rutrum est mollis vitae.</TextContent>
-  </ContentDiv>
- )
 }
 
 function Image({image, alt}){
@@ -71,6 +76,29 @@ function Title({children}) {
 function TextContent({children}) {
   return (
     <p className="text-content">{children}</p>
+  )
+}
+
+function MainContent() {
+  return (
+    <ContentDiv otherClasses={"main-content border"}>
+      <Title>What If?</Title>
+      <TextContent>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed in malesuada libero, eget laoreet lectus. Aliquam finibus dapibus ligula, sit amet tincidunt dui posuere non. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer porttitor tempus tortor ut dictum. Maecenas placerat orci sit amet nisi luctus, porta euismod ipsum sagittis. Nullam nec malesuada diam. Suspendisse eget ante urna.</TextContent>
+      <Image image={camera} alt={"camera"}/>
+      <TextContent>Ut aliquet felis feugiat lorem posuere, id vulputate leo fermentum. Suspendisse vestibulum dolor odio, vitae sollicitudin quam mattis id. Etiam eget eleifend est. Aliquam at iaculis justo. Aenean pulvinar molestie eleifend. Aliquam egestas dignissim mollis. Suspendisse semper risus quis neque ultrices ultricies.</TextContent>
+      <Image image={flower} alt={"flower"}/>
+      <TextContent>Nunc at magna eleifend, luctus lorem vel, bibendum odio. Quisque egestas quam faucibus libero cursus rutrum. Sed a metus placerat, pulvinar nisi at, finibus urna. Duis malesuada quis felis vel tempus. Morbi massa ex, aliquet eget erat eu, tincidunt pulvinar nunc. Quisque lorem est, sollicitudin eget sagittis vitae, faucibus aliquam sapien. Pellentesque vulputate neque diam, ac cursus lorem commodo nec. Curabitur et mauris vitae felis fermentum iaculis. Sed eu urna at nibh convallis sagittis ut eget neque. Vivamus fringilla mauris blandit felis maximus, vel efficitur nisi condimentum. Aliquam vel purus augue. Fusce nulla quam, tempor ac sagittis vel, luctus at quam. Mauris sagittis ac sem vel facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam egestas urna risus, sit amet rutrum est mollis vitae.</TextContent>
+    </ContentDiv>
+  )
+}
+
+function About(){
+  return (
+    <ContentDiv otherClasses={"about-content border"}>
+      <Title>About Our Team</Title>
+      <TextContent></TextContent>
+      <Image image={duck} alt={"camera"}/>
+    </ContentDiv>
   )
 }
 
@@ -103,6 +131,14 @@ class GoTopButton extends React.Component {
       </button>
     )
   }
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      Copyright © 2020
+    </footer>
+  )
 }
 
 export default App;
